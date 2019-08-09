d712189a4007a6a041e991c1f9c6640d9a009da0
Barth <juniior.barth@gmail.com>
2019-08-09 18:36:31 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native Index
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function App () {
+  return (
+    <Text>Hello World</Text>
+  )
+}
